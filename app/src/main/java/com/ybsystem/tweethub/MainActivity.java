a90de68649a7732d2310d19834b8ca755dd77259
ybs32 <hdecogtrexi557960@gmail.com>
2020-10-10 15:50:54 +0900

    [Add] Create new android project 🎉

---
+++ app/src/main/java/com/ybsystem/tweethub/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ybsystem.tweethub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
