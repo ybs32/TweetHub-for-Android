b65e6fa09140a919030631f9b61e0b5e10239582
ybs32 <hdecogtrexi557960@gmail.com>
2020-10-25 21:36:39 +0900

    [Add] Implement floating action buttons

--- app/src/main/java/com/ybsystem/tweethub/activities/MainActivity.java
+++ app/src/main/java/com/ybsystem/tweethub/activities/MainActivity.java
@@ -3,10 +3,22 @@ package com.ybsystem.tweethub.activities;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionButton;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionHelper;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionLayout;
 import com.ybsystem.tweethub.R;
 import com.ybsystem.tweethub.application.TweetHubApp;
+import com.ybsystem.tweethub.libs.rfab.CardItem;
+import com.ybsystem.tweethub.libs.rfab.RapidFloatingActionListView;
 
-public class MainActivity extends ActivityBase {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends ActivityBase
+        implements RapidFloatingActionListView.OnRfaListViewListener {
+
+    // FloatingActionButton
+    private RapidFloatingActionHelper mRfaHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,33 @@ public class MainActivity extends ActivityBase {
         }
 
         setContentView(R.layout.activity_main);
+        setMoreActionButton();
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        mRfaHelper.toggleContent();
+    }
+
+    private void setMoreActionButton() {
+        // Find view
+        RapidFloatingActionLayout rfaLayout = findViewById(R.id.rfal);
+        RapidFloatingActionButton rfaButton = findViewById(R.id.rfab);
+
+        // Create ListView
+        RapidFloatingActionListView rfaListView = new RapidFloatingActionListView(this);
+        rfaListView.setOnRfaListViewListener(this);
+
+        List<CardItem> cardItems = new ArrayList<>();
+        rfaListView.setList(cardItems);
+
+        // Build action button
+        mRfaHelper = new RapidFloatingActionHelper(
+                this,
+                rfaLayout,
+                rfaButton,
+                rfaListView
+        ).build();
     }
 
 }
