29d2dec6a8e5be768d13c8b96fe4b90fc6d86e20
ybs32 <hdecogtrexi557960@gmail.com>
2020-11-23 01:26:41 +0900

    [Fix] Don't using unnecessary layout definition

--- app/src/main/java/com/ybsystem/tweethub/activities/preference/BrowserAuthActivity.java
+++ app/src/main/java/com/ybsystem/tweethub/activities/preference/BrowserAuthActivity.java
@@ -3,7 +3,6 @@ package com.ybsystem.tweethub.activities.preference;
 import android.os.Bundle;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.ybsystem.tweethub.R;
 import com.ybsystem.tweethub.activities.ActivityBase;
 import com.ybsystem.tweethub.fragments.preference.BrowserAuthFragment;
 
@@ -13,14 +12,13 @@ public class BrowserAuthActivity extends ActivityBase {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_fragment);
         setBrowserAuthFragment();
     }
 
     private void setBrowserAuthFragment() {
         BrowserAuthFragment fragment = new BrowserAuthFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.frame_container, fragment);
+        transaction.add(android.R.id.content, fragment);
         transaction.commit();
     }
 }
