a7235db79f64ff2a90fc96270426323e6738ca51
ybs32 <hdecogtrexi557960@gmail.com>
2020-11-22 23:34:14 +0900

    [Remove] Unused import statement

--- app/src/main/java/com/ybsystem/tweethub/adapters/array/DrawerArrayAdapter.java
+++ app/src/main/java/com/ybsystem/tweethub/adapters/array/DrawerArrayAdapter.java
@@ -1,6 +1,5 @@
 package com.ybsystem.tweethub.adapters.array;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
