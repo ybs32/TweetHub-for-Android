394e77464ec595a3b0657b7768cae25fca4ea0d1
ybs32 <hdecogtrexi557960@gmail.com>
2020-11-29 20:06:01 +0900

    [Fix] Remove unnecessary processing

--- app/src/main/java/com/ybsystem/tweethub/libs/photo/PhotoViewPager.java
+++ app/src/main/java/com/ybsystem/tweethub/libs/photo/PhotoViewPager.java
@@ -8,34 +8,21 @@ import androidx.viewpager.widget.ViewPager;
 
 public class PhotoViewPager extends ViewPager {
 
-    private boolean isLocked;
-
     public PhotoViewPager(Context context) {
         super(context);
-        isLocked = false;
     }
 
     public PhotoViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
-        isLocked = false;
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        if (!isLocked) {
-            try {
-                return super.onInterceptTouchEvent(ev);
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
-                return false;
-            }
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            return false;
         }
-        return false;
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return !isLocked && super.onTouchEvent(event);
     }
 
 }
