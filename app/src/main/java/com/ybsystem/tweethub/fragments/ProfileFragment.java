90734d626cf828d2df4ddbc7cd49de268a0ebef3
ybs32 <hdecogtrexi557960@gmail.com>
2020-12-01 22:50:07 +0900

    [Fix] Avoid passing null as the view root

--- app/src/main/java/com/ybsystem/tweethub/fragments/ProfileFragment.java
+++ app/src/main/java/com/ybsystem/tweethub/fragments/ProfileFragment.java
@@ -49,7 +49,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate
-        View view = inflater.inflate(R.layout.fragment_profile, null);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         // Init
         mUser = (TwitterUser) getArguments().getSerializable("USER");
