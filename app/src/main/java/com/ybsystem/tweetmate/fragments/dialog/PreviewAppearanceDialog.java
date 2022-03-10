3b62b6aa1b01d823ea75174604ae60dc280cf7a6
ybs32 <hdecogtrexi557960@gmail.com>
2022-03-11 00:25:30 +0900

    [Add] Layout preview in appearance setting

---
+++ app/src/main/java/com/ybsystem/tweetmate/fragments/dialog/PreviewAppearanceDialog.java
@@ -0,0 +1,39 @@
+package com.ybsystem.tweetmate.fragments.dialog;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.ybsystem.tweetmate.R;
+import com.ybsystem.tweetmate.fragments.timeline.ProfileTimeline;
+import com.ybsystem.tweetmate.models.entities.Column;
+
+import static com.ybsystem.tweetmate.models.enums.ColumnType.USER_TWEET;
+
+public class PreviewAppearanceDialog extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate
+        View view = inflater.inflate(R.layout.dialog_preview_appearance, null);
+
+        // Create timeline
+        Fragment fragment = new ProfileTimeline().newInstance(
+                new Column(7080152L, "", USER_TWEET, false),
+                false
+        );
+
+        // Set timeline
+        FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+        ft.add(R.id.frame_container, fragment);
+        ft.commit();
+
+        return view;
+    }
+
+}
