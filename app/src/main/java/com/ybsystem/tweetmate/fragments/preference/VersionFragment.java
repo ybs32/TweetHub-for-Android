7ad1a94e85ef365ac0e8baca1997e2b27570b2db
ybs32 <hdecogtrexi557960@gmail.com>
2022-03-12 16:47:12 +0900

    [Update] Ver.2.0.0 to 2.0.1

--- app/src/main/java/com/ybsystem/tweetmate/fragments/preference/VersionFragment.java
+++ app/src/main/java/com/ybsystem/tweetmate/fragments/preference/VersionFragment.java
@@ -23,6 +23,10 @@ public class VersionFragment extends Fragment {
         String newVersion = getString(R.string.app_update_info);
         String oldVersion =
                         "\n" +
+                        "Ver.2.0.0\n" +
+                        "・アプリ名とアイコンを刷新\n" +
+                        "・ライセンス表記を追加\n" +
+                        "\n" +
                         "Ver.1.5.1\n" +
                         "・ダークテーマの色を微修正\n" +
                         "・メニューのユーザアイコン表示に対応\n" +
