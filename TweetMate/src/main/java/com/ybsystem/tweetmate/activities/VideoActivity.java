7ce16a108b57919cfb042da23f05ea77c394456e
ybs32 <hdecogtrexi557960@gmail.com>
2022-04-08 00:25:26 +0900

    [Add] Replace VideoView to ExoPlayer library

--- TweetMate/src/main/java/com/ybsystem/tweetmate/activities/VideoActivity.java
+++ TweetMate/src/main/java/com/ybsystem/tweetmate/activities/VideoActivity.java
@@ -2,9 +2,14 @@ package com.ybsystem.tweetmate.activities;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.MediaController;
-import android.widget.VideoView;
 
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
 import com.ybsystem.tweetmate.R;
 import com.ybsystem.tweetmate.models.entities.twitter.TwitterMediaEntity;
 import com.ybsystem.tweetmate.databases.PrefSystem;
@@ -13,18 +18,30 @@ import static com.ybsystem.tweetmate.resources.ResString.*;
 
 public class VideoActivity extends ActivityBase {
 
-    private VideoView mVideoView;
+    // Video player
+    private ExoPlayer mExoPlayer;
+    private PlayerView mPlayerView;
+
+    // Media
+    private TwitterMediaEntity mMediaEntity;
+    private MediaSource mMediaSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Init
-        TwitterMediaEntity media = (TwitterMediaEntity)
+        mMediaEntity = (TwitterMediaEntity)
                 getIntent().getSerializableExtra("MEDIA_ENTITY");
 
+        // Create media
+        String videoURL = PrefSystem.getVideoByQuality(mMediaEntity);
+        mMediaSource = new ProgressiveMediaSource.Factory(
+                new DefaultHttpDataSource.Factory()
+        ).createMediaSource(MediaItem.fromUri(Uri.parse(videoURL)));
+
         // Set actionbar title
-        switch (media.getType()) {
+        switch (mMediaEntity.getType()) {
             case "video":
                 getSupportActionBar().setTitle(STR_MOVIE);
                 break;
@@ -35,27 +52,59 @@ public class VideoActivity extends ActivityBase {
 
         // Set view
         setContentView(R.layout.activity_video);
-        setVideoView(media);
+        setPlayerView();
     }
 
-    private void setVideoView(TwitterMediaEntity media) {
-        // Get video URL by setting
-        String videoURL = PrefSystem.getVideoByQuality(media);
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mExoPlayer != null) {
+            mExoPlayer.setPlayWhenReady(false);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mExoPlayer != null) {
+            mExoPlayer.stop();
+            mExoPlayer.release();
+        }
+    }
+
+    private void setPlayerView() {
+        // Create player
+        mExoPlayer = new SimpleExoPlayer.Builder(this).build();
+        mExoPlayer.setMediaSource(mMediaSource);
+        mExoPlayer.prepare();
+
+        // Set player to view
+        mPlayerView = findViewById(R.id.player_view);
+        mPlayerView.setPlayer(mExoPlayer);
+        mPlayerView.hideController();
 
         // Start video
-        mVideoView = findViewById(R.id.video_view);
-        mVideoView.setMediaController(new MediaController(this));
-        mVideoView.setVideoURI(Uri.parse(videoURL));
-        mVideoView.start();
+        mExoPlayer.setPlayWhenReady(true);
 
         // When completed video
-        mVideoView.setOnCompletionListener(mp -> {
-            // Seek start position
-            mVideoView.seekTo(0);
+        mExoPlayer.addListener(new ExoPlayer.Listener() {
 
-            // When GIF, then repeat
-            if (media.getType().equals("animated_gif")) {
-                mVideoView.start();
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                switch(playbackState) {
+                    case ExoPlayer.STATE_ENDED:
+                        // Seek to start
+                        mExoPlayer.seekTo(0);
+                        mExoPlayer.setPlayWhenReady(false);
+
+                        // If GIF, then repeat
+                        if (mMediaEntity.getType().equals("animated_gif")) {
+                            mExoPlayer.setPlayWhenReady(true);
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
         });
     }
